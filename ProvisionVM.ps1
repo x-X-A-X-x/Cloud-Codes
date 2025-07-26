8919a23f59de37813d00f384d5d2a2b854aa236f
x-X-A-X-x <maximilianbenedictsypausal@yahoo.com>
2025-07-26 09:48:40 +0800

    Adding samples

--- ProvisionVM.ps1
+++ ProvisionVM.ps1
@@ -13,3 +13,13 @@ New-AzVM `
     -Name $vmName `
     -Size $vmSize `
     -Credential (New-Object System.Management.Automation.PSCredential($adminUsername, $adminPassword))
+
+<#
+Calling parameterized script to provision VM:
+
+.\ProvisionVM.ps1 -resourceGroupName "SampleResourceGroup" `
+                  -location "SampleLocation" `
+                  -vmName "SampleVM" `
+                  -adminUsername "Sampleadminuser" `
+                  -adminPassword (ConvertTo-SecureString "SampleP@ssword123" -AsPlainText -Force)
+#>
